52a8c731f8ffdf0578c95dd7c57512ae6a3e6512
serschen <anton.serschen@gmail.com>
2017-03-27 16:44:09 +0200

    added new properties and methods to the data classes

--- app/src/main/java/Data/Database.java
+++ app/src/main/java/Data/Database.java
@@ -1,9 +1,30 @@
 package Data;
 
+import java.util.TreeSet;
+
 /**
  * Created by anton on 27.03.2017.
  */
 
 public class Database {
+    private static Database reference = null;
+    TreeSet<Spieler> tsSpieler = null;
+
+    public Database()
+    {
+        super();
+        tsSpieler = new TreeSet<>();
+    }
+
+    public Database getInstance()
+    {
+        if(reference == null)
+        {
+            reference = new Database();
+        }
+
+        return reference;
+    }
+
 
 }
