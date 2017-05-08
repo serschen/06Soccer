ef65a5e7923c70686439d4a61049d8ce24d0f809
tomaschm <martin_tomii@yahoo.at>
2017-05-08 16:18:04 +0200

    fixed statsfinal

--- app/src/main/java/com/soccer/a06soccer/playerstats_overall.java
+++ app/src/main/java/com/soccer/a06soccer/playerstats_overall.java
@@ -1,8 +1,81 @@
 package com.soccer.a06soccer;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import Data.Database;
+import Data.Player;
+
 /**
  * Created by anton on 26.04.2017.
  */
 
-public class playerstats_overall {
+public class playerstats_overall extends AppCompatActivity {
+    private TextView tvName = null;
+    private TextView tvGoalDefault = null;
+    private TextView tvGoalPenalty = null;
+    private TextView tvGoalHead = null;
+    private TextView tvGoalHeadSnow = null;
+    private TextView tvGoalOwn = null;
+    private TextView tvNutmeg = null;
+    private Database db = null;
+    Player curPlayer = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.playerstats_overall);
+
+        try{
+            db=Database.getInstance();
+            getAllViews();
+            setDetails();
+        }
+        catch(Exception ex)
+        {
+            Toast.makeText(getApplicationContext(),
+                    ex.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    public void getAllViews()
+    {
+        tvName = (TextView) this.findViewById(R.id.txtViewName);
+        tvGoalDefault = (TextView) this.findViewById(R.id.txtViewGoalDefault);
+        tvGoalPenalty = (TextView) this.findViewById(R.id.txtViewGoalPenalty);
+        tvGoalHead = (TextView) this.findViewById(R.id.txtViewGoalHead);
+        tvGoalHeadSnow = (TextView) this.findViewById(R.id.txtViewGoalHeadSnow);
+        tvGoalOwn = (TextView) this.findViewById(R.id.txtViewGoalOwn);
+        tvNutmeg = (TextView) this.findViewById(R.id.txtViewNutmeg);
+    }
+
+    public void setDetails()
+    {
+        curPlayer = db.getCurrentPlayer();
+        tvName.setText(curPlayer.getName());
+
+        /*
+        *setStatsofthePlayerSelected();
+        * */
+    }
+
 }
--- app/src/main/java/com/soccer/a06soccer/statsgui_main.java
+++ app/src/main/java/com/soccer/a06soccer/statsgui_main.java
@@ -1,18 +1,28 @@
 package com.soccer.a06soccer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import Data.Database;
+import Data.Player;
 
 /**
  * Created by anton on 26.04.2017.
  */
 
-public class statsgui_main extends AppCompatActivity {
-    private Database database = null;
+public class statsgui_main extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener{
+    private Button btnShow = null;
+    private ListView lvPlayer = null;
+    private Database db = null;
+    private int curPos = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,29 +31,38 @@ public class statsgui_main extends AppCompatActivity {
         try{
             getAllViews();
             registrateEventHandlers();
-            database = Database.getInstance();
-            getData();
+            db = Database.getInstance();
+
         }
         catch(Exception ex)
         {
             Toast.makeText(getApplicationContext(),
-                    ex.getMessage(), Toast.LENGTH_SHORT)
+                    ex.getMessage(), Toast.LENGTH_LONG)
                     .show();
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, playerstats_overall.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        curPos = position;
+        db.setCurrentPlayer((Player)lvPlayer.getItemAtPosition(curPos));
+    }
+
     public void getAllViews()
     {
-
+        btnShow = (Button) this.findViewById(R.id.btnShow);
+        lvPlayer = (ListView) this.findViewById(R.id.listViewPlayer);
     }
 
     public void registrateEventHandlers()
     {
-
-    }
-
-    public void getData()
-    {
-
+        btnShow.setOnClickListener(this);
+        lvPlayer.setOnItemClickListener(this);
     }
 }
