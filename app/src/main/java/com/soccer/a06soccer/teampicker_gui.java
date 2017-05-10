fafadb2ddfdc7111bb2c347bcb2ebc91dacea833
tomaschm <martin_tomii@yahoo.at>
2017-05-10 08:30:49 +0200

    teampicker

---
+++ app/src/main/java/com/soccer/a06soccer/teampicker_gui.java
@@ -0,0 +1,74 @@
+package com.soccer.a06soccer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.ListView;
+import android.widget.Button;
+import Data.Database;
+import Data.Player;
+
+/**
+ * Created by Martin on 08.05.2017.
+ */
+
+public class teampicker_gui extends AppCompatActivity  implements View.OnClickListener,AdapterView.OnItemClickListener {
+    private ListView ListViewSelectedPlayer = null;
+    private ListView ListViewAllPlayer = null;
+    private Button btnAdd = null;
+    private Button btnRemove = null;
+    private Database db = null;
+    private Player curPlayer = null;
+    private int curPos = -1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statsgui_main);
+
+        try{
+            getAllViews();
+            registrateEventHandlers();
+            db = Database.getInstance();
+            loadPlayer();
+        }
+        catch(Exception ex)
+        {
+            Toast.makeText(getApplicationContext(),
+                    ex.getMessage(), Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    public void getAllViews()
+    {
+        ListViewSelectedPlayer = (ListView) this.findViewById(R.id.lvSelectedPlayer);
+        ListViewAllPlayer = (ListView) this.findViewById(R.id.lvAllPlayer);
+        btnAdd = (Button) this.findViewById(R.id.bttnAdd);
+        btnRemove = (Button) this.findViewById(R.id.bttnRemove);
+    }
+
+    public void registrateEventHandlers()
+    {
+        ListViewSelectedPlayer.setOnItemClickListener(this);
+        ListViewAllPlayer.setOnItemClickListener(this);
+        btnAdd.setOnClickListener(this);
+        btnRemove.setOnClickListener(this);
+    }
+    private void loadPlayer()
+    {
+
+    }
+}
